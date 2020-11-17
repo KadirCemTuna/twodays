e50deb4151dfb6cdac7081ff8b0be08cc4483e46
kadircemtuna <kadir.tuna@etiya.com>
2020-11-17 11:12:16 +0300

    New data type for Consume API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Article, GoogleNews} from '../model/GoogleNews';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +8,22 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  newsArr: Article[];
   title = 'twodays';
   greenColor = 'green';
   redColor = 'red';
   focusStatus = '';
   dataFocus = '';
 
-  constructor( private http: HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
-funcSendBtn(): void{
+  funcSendBtn(): void {
     const url = 'http://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=0daa40ca50444db4884a60637002c6fc';
-    this.http.get(url).subscribe(items => {
-      console.log('items: ' + JSON.stringify(items));
+    this.http.get(url).subscribe(item => {
+      const gNew: GoogleNews | any = item;
+      this.newsArr = gNew.articles;
+      console.log(this.newsArr);
     });
   }
 }
